ad9013ea62421a5ac10f1d96e3fd951123dc8f48
Restyled.io <commits@restyled.io>
2021-05-08 03:12:42 +0000

    Restyled by clang-format

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,34 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-  entry: [
+  entry : [
     './src/_includes/js/custom.js',
     './src/_includes/css/style.css',
     './src/_includes/css/responsive.css',
   ],
-  module: {
-    rules: [
+  module : {
+    rules : [
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader?url=false'],
+        test : /\.css$/i,
+        use : [ MiniCssExtractPlugin.loader, 'css-loader?url=false' ],
       },
     ],
   },
-  optimization: {
-    minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+  optimization : {
+    minimizer : [
+      // For webpack@5 you can use the `...` syntax to extend existing
+      // minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
       // `...`,
       new CssMinimizerPlugin(),
     ],
   },
-  plugins: [
+  plugins : [
     new MiniCssExtractPlugin({
-      filename: '../css/[name].css',
+      filename : '../css/[name].css',
     }),
   ],
-  output: {
-    filename: 'custom.js',
-    path: path.resolve(__dirname, 'dist/js'),
+  output : {
+    filename : 'custom.js',
+    path : path.resolve(__dirname, 'dist/js'),
   },
 };
