8adad78c4ed4372367c3288ef81e5886c5af4ef8
Josh Moyers <josh@moyers.dev>
2021-05-07 23:09:38 -0400

    Optimizing assets and leveraging CDNs

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,16 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({"src/_includes/css": "css"});
-    eleventyConfig.addPassthroughCopy({"src/_includes/js": "js"});
-    eleventyConfig.addPassthroughCopy({"src/_includes/font": "font"});
-    eleventyConfig.addPassthroughCopy({"src/_includes/fonts": "fonts"});
-    eleventyConfig.addPassthroughCopy({"src/_includes/webfonts": "webfonts"});
-    eleventyConfig.addPassthroughCopy({"src/images": "images"});
-
-    return {
-      markdownTemplateEngine: 'njk',
-      dataTemplateEngine: 'njk',
-      htmlTemplateEngine: 'njk',  
-      dir: {
-        input: 'src',
-        output: 'dist'
-      }
-    }
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ 'src/_includes/font': 'font' });
+  eleventyConfig.addPassthroughCopy({ 'src/_includes/fonts': 'fonts' });
+  eleventyConfig.addPassthroughCopy({ 'src/_includes/webfonts': 'webfonts' });
+  eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
+  eleventyConfig.setWatchThrottleWaitTime(250);
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
   };
+};
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/_includes/js/custom.js',
+    './src/_includes/css/style.css',
+    './src/_includes/css/responsive.css',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader?url=false'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../css/[name].css',
+    }),
+  ],
+  output: {
+    filename: 'custom.js',
+    path: path.resolve(__dirname, 'dist/js'),
+  },
+};
